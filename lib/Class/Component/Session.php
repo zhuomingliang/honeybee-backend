ea805b9ab46ddc98337bd2014fcdbe8ced12bfc0
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2010-07-31 22:14:55 +0800

    added Session class

---
+++ lib/Class/Component/Session.php
@@ -0,0 +1,27 @@
+<?php
+Class Session {
+    public static function open() {
+        
+    }
+    
+    public static function close() {
+        
+    }
+    
+    public static function read() {
+        
+    }
+    
+    public static function write() {
+        
+    }
+    
+    public static function destroy() {
+        
+    }
+    
+    public static function gc() {
+        
+    }
+}
+?>
