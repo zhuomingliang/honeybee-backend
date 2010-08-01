33983cd6957fcec80981261ab37e38cdc5f84cf8
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2010-08-01 12:40:35 +0800

    fixed bug

--- lib/Class/Core/Base/MongoDBi.php
+++ lib/Class/Core/Base/MongoDBi.php
@@ -8,15 +8,17 @@ class MongoDBi {
         if (isset(self::$_MongoDBConnections[$db])) {
             return self::$_MongoDBConnections[$db];
         }
-        
+
         if ($db === '_default') {
             $config = Config::get('MongoDB');
             if (!isset($config['db'])) {
                 throw new Exception('There is no DB config in ini file, do you miss it?');
             }
+
+            return self::$_MongoDBConnections[$db] = Mongoi::getConnection()->selectDB($config['db']);
         }
 
-        return self::$_MongoDBConnections[$db] = Mongoi::getConnection()->selectDB($config['db']);
+        return self::$_MongoDBConnections[$db] = Mongoi::getConnection()->selectDB($db);
       }
 }
 ?>
