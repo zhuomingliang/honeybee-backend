56b10c49c08645e1ea8ccf4403be7693c0769d0b
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-18 17:18:50 +0800

    fix

--- config/lib.config.php
+++ config/lib.config.php
@@ -3,7 +3,6 @@ if (!defined('HONEYBEE_DIR')) {
     define('HONEYBEE_DIR', dirname(dirname(__FILE__)) . '/');
 }
 require HONEYBEE_DIR . 'include/database/database.inc';
-require HONEYBEE_DIR . 'include/database/log.inc';
 require HONEYBEE_DIR . 'lib/Class/Core/Base/autoloader.php';
 require HONEYBEE_DIR . 'lib/Class/Core/Controller/Controller.php';
 require HONEYBEE_DIR . 'lib/Class/Core/Controller/AppController.php';
